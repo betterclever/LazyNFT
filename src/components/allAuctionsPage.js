abc7d811a3909c06d25c568ff126db1255a5b424
Nishtha Paliwal <paliwal.nishtha@gmail.com>
2021-06-11 00:13:00 +0530

    ui wip

--- src/components/allAuctionsPage.js
+++ src/components/allAuctionsPage.js
@@ -1,7 +1,7 @@
 import {TopBar} from "./topBar";
 import Countdown from 'react-countdown';
 export function ItemCard({item}) {
-    const {collectionName, currentPrice,collectionId, endTime}=item;
+    const {collectionName, currentPrice,collectionId, endTime, status=''}=item;
     function getColorById(){
       let h = collectionId % 360;
       return 'hsl(' + h + ', ' + 90 + '%, ' + 90 + '%)';
@@ -13,7 +13,6 @@ export function ItemCard({item}) {
             <div className="flex flex-row justify-between m-2" style={{justifyContent:'center'}}>
                 <div className="flex flex-col">
                     <span className="text-xl"> {collectionName} </span>
-                    <span className="text-xl">  </span>
                     <div className="mt-20">
                         <span className="text-gray-800"> Current Price:  </span>
                         <span className="text-blue-800 mt-10"> {currentPrice} </span>
@@ -23,6 +22,9 @@ export function ItemCard({item}) {
                           daysInHours={true}/>
                         </div>
                     <div> 
+                    {status!=='' && <div className=" mt-2" style={{flexDirection:'row',textAlign:'center'}}>
+                                        <span className="text-green-800 mt-10"> {status} </span>
+                                    </div>}
                 </div>
             </div>
         </div>
@@ -32,9 +34,9 @@ export function ItemCard({item}) {
 
 export function AuctionsGrid() {
     return <div className="grid grid-cols-5 gap-20 ml-20 mr-20 mt-20">
-        <ItemCard item={{collectionId:'563236',collectionName:'Instagram NFTed', currentPrice:'100 ZIL', endTime:''}}/>
+        <ItemCard item={{collectionId:'563236',collectionName:'Instagram NFTed', currentPrice:'100 ZIL', endTime:'',status:"Claimed"}} />
         <ItemCard item={{collectionId:'5674536',collectionName:'Instagram NFTed', currentPrice:'100 ZIL', endTime:''}}/>
-        <ItemCard item={{collectionId:'5644336',collectionName:'Instagram NFTed', currentPrice:'100 ZIL', endTime:''}}/>
+        <ItemCard item={{collectionId:'5644336',collectionName:'Instagram NFTed', currentPrice:'100 ZIL', endTime:'', status:"Claimed"}}/>
         <ItemCard item={{collectionId:'53236',collectionName:'Instagram NFTed', currentPrice:'100 ZIL', endTime:''}}/>
         <ItemCard item={{collectionId:'563243',collectionName:'Instagram NFTed', currentPrice:'100 ZIL', endTime:''}}/>
         <ItemCard item={{collectionId:'43236',collectionName:'Instagram NFTed', currentPrice:'100 ZIL', endTime:''}}/>
